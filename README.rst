691c610c52e564ddcefc7f9872cddaa77f8c1f79
jqb <kuba.janoszek@gmail.com>
2015-03-22 02:22:32 +0100

    "def_block" and "use_block" macros (sorf of) added

--- README.rst
+++ README.rst
@@ -20,8 +20,8 @@ Installation
 
 
 
-Usage: render_with
-------------------
+Usage: render_with & def_block and use_block
+--------------------------------------------
 
 This is sort of macro-like solution allowing you to define base
 template that looks always the same, and fill just a "block" inside it
@@ -29,30 +29,70 @@ differently.
 
 Assuming you have partial template person_wrapper.html::
 
+
+  {% load goodies %}
   <div class="person-wrapper">
       <div class="first-name">{{ first_name }}</div>
-      {{ block }}
+      {% use_block "block" %}
   </div>
 
 
 You can use it in following way::
 
-  {% load goodies %}
 
+  {% load goodies %}
   {% render_with "person_wrapper.html" first_name="First" last_name="Last" %}
-  <div class="last-name">{{ last_name }}</div>
+      {% def_block "block" %}
+      <div class="last-name">{{ last_name }}</div>
+      {% end %}
   {% end %}
 
 
-This invocation will substitude "{{ block }}" in person_wrapper.html
-with the contents within "{% render_with .... %}" and "{% end %}". So
-the outcome of above will be more or less::
+This invocation will substitude "{% use_block "block" %}" in
+person_wrapper.html with the contents within "{% def_block .... %}"
+and "{% end %}" insidee "render_with". So the outcome of above will be
+more or less::
+
 
   <div class="person-wrapper">
       <div class="first-name">First</div>
       <div class="last-name">Last</div>
   </div>
 
+
+Another example with tables, table.html::
+
+
+  {% load goodies %}
+  <table class="{{ opts.table_class }}">
+      <thead>
+          <tr>
+              {% use_block "headers" %}
+          </tr>
+      </thead>
+      <tbody>
+          {% for row in object_list %}
+          <tr>
+              {% use_block "row" %}
+          </tr>
+          {% endfor %}
+      </tbody>
+  </table>
+
+
+can be used in following way::
+
+  {% render_with "table.html" object_list=people %}{% def_block "headers" %}
+      <td>ID</td>
+      <td>First name</td>
+      <td>Last Name</td>
+      <td>Age</td>
+  {% end %}{% def_block "row" %}
+      <td>{{ forloop.counter }}</td>
+      <td>{{ row.first_name }}</td>
+      <td>{{ row.last_name }}</td>
+      <td>{{ row.age }}</td>
+  {% end %}{% end %}
 
 
 Usage: dict
--- template_goodies/templatetags/goodies.py
+++ template_goodies/templatetags/goodies.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import copy
-from cStringIO import StringIO
 
 from django import template
 from django.template.loader import render_to_string
@@ -17,17 +16,62 @@ register = template.Library()
 
 
 class Block(object):
-    def __init__(self, nodes, context):
+    def __init__(self, nodes, context, name=None):
+        self.name = name
         self.nodes = nodes
         self._context = context
 
     def __unicode__(self):
-        buf = StringIO()
-        for n in self.nodes:
-            buf.write(n.render(self._context))
-        return buf.getvalue()
+        return self.nodes.render(self._context)
 
     __str__ = __repr__ = __unicode__
+
+
+class DefBlock(Tag):
+    options = Options(
+        Argument('name', resolve=True),
+        MultiKeywordArgument('kwargs', required=False, resolve=True),
+        blocks=[
+            BlockDefinition('nodelist', 'end')
+        ]
+    )
+
+    def render_tag(self, context, name, kwargs, nodelist):
+        def_block = context.get('def_block', {})
+        context['def_block'] = def_block
+        context = copy.copy(context)
+        context.update(kwargs)
+
+        for n in nodelist:
+            if isinstance(n, DefBlock):
+                n.render(context)
+
+        def_block[name] = Block(nodelist, context, name=name)
+        return ''
+
+
+register.tag(DefBlock)
+
+
+class UseBlock(Tag):
+    options = Options(
+        Argument('name', resolve=True),
+        MultiKeywordArgument('kwargs', required=False, resolve=True),
+    )
+
+    def render_tag(self, context, name, kwargs):
+        def_block = context.get('def_block', {})
+        context['def_block'] = def_block
+
+        block = def_block[name]
+        ctx = copy.copy(block._context)
+        ctx.update(context)
+        ctx.update(kwargs)
+
+        return block.nodes.render(ctx)
+
+
+register.tag(UseBlock)
 
 
 class RenderWith(Tag):
@@ -42,6 +86,11 @@ class RenderWith(Tag):
     def render_tag(self, context, template, kwargs, nodelist):
         context = copy.copy(context)
         context.update(kwargs)
+
+        for n in nodelist:
+            if isinstance(n, DefBlock):
+                n.render(context)
+
         context['block'] = Block(nodelist, context)
         return render_to_string(template, context)
 
